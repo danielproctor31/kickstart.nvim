aa916b9879abdd9bec0e686f8cf73fdd680f5ba3
Daniel Proctor <dan@danielproctor.dev>
2026-02-04 23:12:26 +0000

    add js and ts lsp

--- init.lua
+++ init.lua
@@ -107,7 +107,7 @@ vim.o.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
--- Don't show the mode, since it's already in the status line
+-- Don't show the mode, since it's already in the status line:
 vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
@@ -603,6 +603,13 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        vtsls = {},
+        eslint = {
+          settings = {
+            -- tells eslint to run on every save
+            workingDirectories = { mode = 'auto' },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -612,11 +619,16 @@ require('lazy').setup({
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
+local ensure_installed = vim.tbl_filter(function(key)
+        return key ~= 'eslint' 
+      end, vim.tbl_keys(servers or {}))
       vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
+        'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
+        'eslint-lsp',
+        -- 'typescript-language-server',
+        'vtsls',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -691,6 +703,11 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        
       },
     },
   },
